3e14e3b0c81c2f5a7f940666075f61b971791e6e
karthisanjaykk <karthikoms10>
2018-05-29 12:59:45 +0530

    used opprators

---
+++ opprators.java
@@ -0,0 +1,13 @@
+public class opprators{
+public static void main(String[] args){
+ int k=23;
+ int l=34;
+ System.out.println((k>l) +"-greater");
+ System.out.println((k<l) +"-lesser");
+ System.out.println((k&l) +" -and");
+ System.out.println((k|l) +"-or");
+ System.out.println((k+l) +"-add");
+ System.out.println(k/l);
+ System.out.println(l%k);
+ 
+ }}
