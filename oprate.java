00b77b05ca701b439b154afc8558e93eec88160d
karthisanjaykk <39474897+karthisanjaykk@users.noreply.github.com>
2018-05-31 18:41:02 +0530

    Add files via upload

---
+++ oprate.java
@@ -0,0 +1,9 @@
+package datatype;
+
+public class oprate {
+	public static void main(String[]args) {
+		int num =(5>6)? 10:20;
+		System.out.println(num);
+	}
+
+}
---
+++ whiles.java
@@ -0,0 +1,12 @@
+package datatype;
+
+public class whiles {
+	public static void main(String[]args) {
+		int k=1;
+		while(k<=10) {
+			System.out.println("k=" + k);
+			k++;
+		}
+	}
+
+}
